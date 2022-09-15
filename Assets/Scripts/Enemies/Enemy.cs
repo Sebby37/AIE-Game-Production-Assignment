9a45da53509bcd88ee4741604269758849aa78e9
Sebastian Cramond <s214178@students.aie.edu.au>
2022-09-15 20:16:55 +0930

    Begun work on enemy script and slime behaviour

---
+++ Assets/Scripts/Enemies/Enemy.cs
@@ -0,0 +1,68 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+/*
+  How to 
+ [System.Serializable]
+ public class TestCategory
+ {
+     public string name;
+ }
+ public TestCategory coolTest;
+*/
+
+public enum EnemyState
+{
+    Idle,
+    Pursuit,
+    Attacking,
+    Damaged,
+    Dying
+}
+
+public class Enemy : MonoBehaviour
+{
+    [Header("Enemy Settings")]
+    public float health;
+    public float aggroRange;
+
+    float maxHealth;
+    GameObject playerObject;
+    [SerializeField] EnemyState state = EnemyState.Idle;
+
+    [Header("Idle Behaviour")]
+    public float timeBetweenIdleBehaviour;
+    public UnityEvent idleBehaviour;
+
+    [Header("Player Pursuit Behaviour")]
+    public float timeBetweenPlayerPursuit;
+    public UnityEvent playerPursuitBehaviour;
+
+    [Header("Attack Behaviour")]
+    public float timeBetweenAttacks;
+    public UnityEvent attackBehaviour;
+
+    [Header("Damage Behaviour")]
+    public UnityEvent damageBehaviour;
+
+    [Header("Death Behaviour")]
+    public UnityEvent deathBehaviour;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        maxHealth = health;
+        playerObject = GameObject.FindGameObjectWithTag("Player");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Vector2.Distance(transform.position, playerObject.transform.position) <= aggroRange)
+        {
+            state = EnemyState.Pursuit;
+        }
+    }
+}
---
+++ Assets/Scripts/Enemies/SlimeBehaviour.cs
@@ -0,0 +1,46 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SlimeBehaviour : MonoBehaviour
+{
+    private void Start()
+    {
+        
+    }
+
+    private void Update()
+    {
+        
+    }
+
+    // The idle behaviour of the Slime
+    public void Idle()
+    {
+
+    }
+    
+    // The behaviour to pursue the player
+    public void PursuePlayer()
+    {
+
+    }
+
+    // The behaviour when damaged
+    public void Damaged()
+    {
+
+    }
+
+    // The death behaviour
+    public void Die()
+    {
+
+    }
+
+    // A function to leap to a position
+    void Leap(Vector3 landingPosition, float leapTime)
+    {
+
+    }
+}
