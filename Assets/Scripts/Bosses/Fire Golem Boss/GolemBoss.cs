0266b4a69e75b5581175a2d48853a304b3a8e0be
sebby37 <47074056+Sebby37@users.noreply.github.com>
2022-11-10 16:04:36 +1030

    Began work on Golem Boss Attack type 2

--- Assets/Scripts/Bosses/Fire Golem Boss/GolemBoss.cs
+++ Assets/Scripts/Bosses/Fire Golem Boss/GolemBoss.cs
@@ -14,20 +14,58 @@ public enum GolemStates
 public class GolemBoss : MonoBehaviour
 {
     [Header("State Information")]
-    public GolemStates state;
+    public GolemStates state = GolemStates.Idle;
     public bool inBossBattle = false;
+
+    [Header("Animation Config")]
+    public float animationSpeed = 1.0f;
+    public int animationSampleRate = 24;
+
+    [Header("Attack 2 Config")]
+    public int groundPoundFrame = 8;
+
+    Animator animator;
     
     // Start is called before the first frame update
     void Start()
     {
-        
+        animator = GetComponent<Animator>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        // Setting the animation speed variable in the animation controller each frame
+        animator.SetFloat("Speed", animationSpeed);
     }
 
-    // Function to begin the boss battle
+    // Function to begin Attack 2 - Ground Pound
+    [ContextMenu("Attack 2")]
+    void Attack2()
+    {
+        // Returning if the current state is not idle
+        if (state != GolemStates.Idle)
+            return;
+        
+        // Setting the state
+        state = GolemStates.Attacking;
+
+        // Triggering the animation
+        animator.SetTrigger("Attack 2");
+
+        // Beginning the attack coroutine
+        StartCoroutine(Attack2Coroutine());
+    }
+
+    IEnumerator Attack2Coroutine()
+    {
+        // Waiting until the ground pound is to happen
+        float timeToWait = ((float) groundPoundFrame / (float) animationSampleRate) * (1 / animationSpeed);
+        print(timeToWait);
+        yield return new WaitForSeconds(timeToWait);
+
+        print("Ground pound!!!");
+        // Setting the attack state to idle as the attack is complete
+        state = GolemStates.Idle;
+    }
 }
