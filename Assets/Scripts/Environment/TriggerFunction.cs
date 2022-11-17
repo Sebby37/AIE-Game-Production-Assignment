23b7cd69b97c91472918d7e1f78182ea9d23bfc7
sebby37 <47074056+Sebby37@users.noreply.github.com>
2022-11-17 16:48:37 +1030

    Increase range of functionality for trigger function script to include colliders and onStay/onExit

--- Assets/Scripts/Environment/TriggerFunction.cs
+++ Assets/Scripts/Environment/TriggerFunction.cs
@@ -5,23 +5,83 @@ using UnityEngine.Events;
 
 public class TriggerFunction : MonoBehaviour
 {
-    [Header("Config")]
-    public UnityEvent eventToTrigger;
-    public bool destroyOnTrigger = true;
+    [Header("Tags")]
     public List<string> triggerTags = new List<string>();
 
-    private void OnTriggerEnter2D(Collider2D collision)
+    [Header("Events")]
+    public UnityEvent onEnterEvent;
+    public UnityEvent onStayEvent;
+    public UnityEvent onExitEvent;
+
+    // The onEnter function
+    void Enter(GameObject collision)
     {
         // Running the event if the collider's tag is in the list of tags
         if (triggerTags.Contains(collision.tag))
         {
             // Triggering the event
-            if (eventToTrigger != null)
-                eventToTrigger.Invoke();
-
-            // Destroying the trigger gameobject if set to do so
-            if (destroyOnTrigger)
-                Destroy(gameObject);
+            if (onEnterEvent != null)
+                onEnterEvent.Invoke();
         }
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        Enter(collision.gameObject);
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        Enter(collision.gameObject);
+    }
+
+    // The onStay function
+    void Stay(GameObject collision)
+    {
+        // Running the event if the collider's tag is in the list of tags
+        if (triggerTags.Contains(collision.tag))
+        {
+            // Triggering the event
+            if (onStayEvent != null)
+                onEnterEvent.Invoke();
+        }
+    }
+
+    private void OnCollisionStay2D(Collision2D collision)
+    {
+        Stay(collision.gameObject);
+    }
+
+    private void OnTriggerStay2D(Collider2D collision)
+    {
+        Stay(collision.gameObject);
+    }
+
+    // The onExit function
+    void Exit(GameObject collision)
+    {
+        // Running the event if the collider's tag is in the list of tags
+        if (triggerTags.Contains(collision.tag))
+        {
+            // Triggering the event
+            if (onExitEvent != null)
+                onEnterEvent.Invoke();
+        }
+    }
+
+    private void OnCollisionExit2D(Collision2D collision)
+    {
+        Exit(collision.gameObject);
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        Exit(collision.gameObject);
+    }
+
+    // A function to destroy the gameobject
+    public void DestroySelf()
+    {
+        Destroy(gameObject);
     }
 }
